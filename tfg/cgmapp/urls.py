ff34649de153a21a48e6174046ed3982cc14d3d0
manuel <m.perezp@udc.es>
2018-05-10 12:19:24 +0200

    Nuevas vistas: login y register

--- tfg/cgmapp/urls.py
+++ tfg/cgmapp/urls.py
@@ -1,1 +1,9 @@
-from django.http import HttpResponse
+from django.conf.urls import include,patterns,url
+from cgmapp import views
+
+urlpatterns = patterns('', 
+	url(r'^register/', views.register, name='register'),
+	url(r'^login/', views.login, name='login'),#pantalla de login
+	url(r'^', include('django.contrib.auth.urls')),
+	url(r'^$', views.index, name='index'),
+	)
